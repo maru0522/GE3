e4826826e1507557b278753384a59882d8cc8c21
maru0522 <94513876+maru0522@users.noreply.github.com>
2022-12-26 21:06:13 +0900

    billboard追加しようと思ったけど挫折

--- Billborad.h
+++ Billborad.h
@@ -1,5 +1,34 @@
 #pragma once
+#include <array>
+#include <DirectXMath.h>
+#include <wrl.h>
+#include "ConstBuffer.h"
+#include <string>
+#include "Window.h"
+#if _MSC_VER > 1922 && !defined(_SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING)
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#endif
+#include <experimental/filesystem>
+#include "GraphicsPipeline.h"
+#include <d3d12.h>
+
+#pragma comment(lib,"d3d12.lib")
+
 class Billborad
 {
+private: // 定義
+    // エイリアステンプレート
+    template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+private: // 変数
+    // テクスチャ情報の引き出しハンドル
+    D3D12_GPU_DESCRIPTOR_HANDLE srvGpuHandleCopy_{};
+    DirectX::XMFLOAT2 originalSize_{}; // テクスチャの全体サイズ
+
+#pragma region 頂点
+    //std::array<VertexSprite_st, 4> vertices_{}; // 頂点データ
+    ComPtr<ID3D12Resource> vertBuff_{ nullptr }; // 頂点バッファの生成
+    D3D12_VERTEX_BUFFER_VIEW vbView_{}; // 頂点バッファビューの作成
+#pragma endregion
 };
 

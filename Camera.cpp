0fc836bc3c5b723fd5bfe9a87acf5dfd8d83e572
maru0522 <94513876+maru0522@users.noreply.github.com>
2023-01-07 03:04:30 +0900

    projectionState_をOrthoGraphicに変更する際の引数に、nearZ_とfarZ_を使用し、値を変更して運用するように修正した。Modified to use nearZ_ and farZ_ as arguments when changing projectionState_ to OrthoGraphic, and change the values ​​to operate.

--- Camera.cpp
+++ Camera.cpp
@@ -13,11 +13,13 @@ void Camera::SetProjection(Camera::Projection projectionType)
 {
     if (projectionType == Camera::Projection::ORTHOGRAPHIC) {
         // •½s“Š‰e
-        matProjection_ = DirectX::XMMatrixOrthographicOffCenterLH(0.0f, Window::width_, Window::height_, 0.0f, 0.0f, 1.0f);
+        nearZ_ = 0.0f, farZ_ = 1.0f;
+        matProjection_ = DirectX::XMMatrixOrthographicOffCenterLH(0.0f, Window::width_, Window::height_, 0.0f, nearZ_, farZ_);
         projectionState_ = Camera::Projection::ORTHOGRAPHIC; // ORTHOGRAPHIC
     }
     else {
         // “§Ž‹“Š‰e
+        nearZ_ = 0.1f, farZ_ = 1000.0f;
         matProjection_ = DirectX::XMMatrixPerspectiveFovLH(Util::Convert::ToRadian(45.0f), (float)Window::width_ / Window::height_, nearZ_, farZ_);
         projectionState_ = Camera::Projection::PERSPECTIVE; // PERSPECTIVE
     }
@@ -27,12 +29,14 @@ void Camera::SwitchProjection(void)
 {
     if (projectionState_ == Camera::Projection::ORTHOGRAPHIC) {
         // “§Ž‹“Š‰e
+        nearZ_ = 0.1f, farZ_ = 1000.0f;
         matProjection_ = DirectX::XMMatrixPerspectiveFovLH(Util::Convert::ToRadian(45.0f), (float)Window::width_ / Window::height_, nearZ_, farZ_);
         projectionState_ = Camera::Projection::PERSPECTIVE; // PERSPECTIVE
     }
     else {
         // •½s“Š‰e
-        matProjection_ = DirectX::XMMatrixOrthographicOffCenterLH(0.0f, Window::width_, Window::height_, 0.0f, 0.0f, 1.0f);
+        nearZ_ = 0.0f, farZ_ = 1.0f;
+        matProjection_ = DirectX::XMMatrixOrthographicOffCenterLH(0.0f, Window::width_, Window::height_, 0.0f, nearZ_, farZ_);
         projectionState_ = Camera::Projection::ORTHOGRAPHIC; // ORTHOGRAPHIC
     }
 }
